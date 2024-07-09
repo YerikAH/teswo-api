6dff7049a6e5f09c95a81102c3ced8dea966e652
Harvey Yerik <yerik05vh@gmail.com>
2024-07-09 11:49:59 -0500

    :sparkles: Add new logic in service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      throw new InternalServerErrorException('Ayuda');
+    }
   }
 
   findAll() {
