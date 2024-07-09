efc4a42c2f26cb6fd7548493f4d78b9afebf7ee0
Harvey Yerik <yerik05vh@gmail.com>
2024-07-09 10:22:36 -0500

    :sparkles: Add postgres config

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
+
+# TesWo API
+
+1. Levantar la base de datos
+
+```sh
+docket-compose up -d
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true, // para cargar automaticamente las entidades
+      synchronize: true, // si eliminar una columna automaticamente se sincroniza, en producción no se usa, solo en desarollo
+    }),
+  ],
   controllers: [],
   providers: [],
 })
