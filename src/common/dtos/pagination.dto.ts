2293d6ae1154fff7ab0a717a9196038035d31131
Harvey Yerik <yerik05vh@gmail.com>
2024-08-02 23:16:41 -0500

    :sparkles: Add some

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,13 +1,26 @@
 /* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'Limit of records to return',
+    example: 10,
+    minimum: 1,
+  })
   @IsOptional()
   @IsPositive()
   @Type(() => Number)
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description: 'Offset of records to return',
+    example: 0,
+    minimum: 0,
+  })
   @IsOptional()
   @Min(0)
   @Type(() => Number)
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -11,41 +12,89 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product title (unique)',
+    nullable: false,
+    minLength: 1,
+    example: 'Product 1',
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty({
+    description: 'Product price',
+    nullable: false,
+    minimum: 0,
+    example: 100,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price: number;
 
+  @ApiProperty({
+    description: 'Product description',
+    nullable: true,
+    example: 'Product 1 description',
+  })
   @IsString()
   @IsOptional()
   description: string;
 
+  @ApiProperty({
+    description: 'Product slug (unique)',
+    nullable: true,
+    example: 'product-1',
+  })
   @IsString()
   @IsOptional()
   slug: string;
 
+  @ApiProperty({
+    description: 'Product stock',
+    nullable: true,
+    minimum: 0,
+    example: 10,
+  })
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock: number;
 
+  @ApiProperty({
+    description: 'Product tags',
+    nullable: true,
+    example: ['tag1', 'tag2'],
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiProperty({
+    description: 'Product sizes',
+    nullable: true,
+    example: ['S', 'M', 'L'],
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   sizes: string[];
 
+  @ApiProperty({
+    description: 'Product gender',
+    nullable: true,
+    example: 'men',
+  })
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty({
+    description: 'Product images',
+    nullable: true,
+    example: ['image1.jpg', 'image2.jpg'],
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
